a84c31c01b5aa913b3915a4927ca67a9b3796337
Smer4s <studnikita@gmail.com>
2024-02-16 18:07:08 +0300

    initial commit

--- Utils/CustomFormatProvider.cs
+++ Utils/CustomFormatProvider.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
 namespace DateJournal.Utils
 {
-	internal class FormatProvider
+	public class CustomFormatProvider : IFormatProvider
 	{
+		public object? GetFormat(Type? formatType)
+		{
+			if (formatType == typeof(DateTimeFormatInfo))
+			{
+				var dateTimeFormatInfo = (DateTimeFormatInfo)CultureInfo.InvariantCulture.DateTimeFormat.Clone();
+				dateTimeFormatInfo.ShortDatePattern = "dd.MM.yyyy";
+				dateTimeFormatInfo.ShortTimePattern = "HH:mm";
+				return dateTimeFormatInfo;
+			}
+
+			return null;
+		}
 	}
 }
